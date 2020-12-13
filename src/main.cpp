b8e2d50bd5e05042a327a9faec5ea2092fc18f14
Curtis Malainey <cujomalainey@chromium.org>
2020-12-12 23:18:06 -0800

    initial code commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+/**
+ * Simple single button control for Neopixel
+ *
+ * Short press: change mode/on
+ * Long press: change brightness
+ * Double tap: off
+ */
+
+void setup(){}
+
+void loop(){}
+
